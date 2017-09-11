ee28fec079fc7b0c3abcbd5343f144d008b69106
Kevin Kodasma <kodakevi@greeny.cs.tlu.ee>
2017-09-11 10:37:06 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+	<meta charset="utf-8">
+	<title>Kevin Kodasma</title>
+</head>
+<body>
+	<h1>Kevin Kodasma</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	
+	<h2>Absoluutselt</h2>
+	<p>Aeg on uueks linnapeaks.</p>
+	
+	<?php
+		echo "Täna on vastik ilm!";
+	?>
+	
+	</body>
+</html>
